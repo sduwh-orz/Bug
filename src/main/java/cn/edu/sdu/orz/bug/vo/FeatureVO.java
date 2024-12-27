1c97c8e95b381a208861527e1d7ac2c6514a9bbb
xszqxszq <943551369@qq.com>
2024-12-27 12:50:16 +0800

    protected

--- src/main/java/cn/edu/sdu/orz/bug/vo/FeatureVO.java
+++ src/main/java/cn/edu/sdu/orz/bug/vo/FeatureVO.java
@@ -12,25 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @SuppressWarnings("unused")
 public class FeatureVO {
-    private String name;
+    protected String name;
 
-    private BigDecimal hours;
-
-    /**
-     * Instantiates a new Feature vo.
-     */
-    public FeatureVO() {
-        super();
-    }
-
-    /**
-     * Instantiates a new Feature vo.
-     *
-     * @param name  the name
-     * @param hours the hours
-     */
-    public FeatureVO(String name, BigDecimal hours) {
-        setName(name);
-        setHours(hours);
-    }
+    protected BigDecimal hours;
 }
